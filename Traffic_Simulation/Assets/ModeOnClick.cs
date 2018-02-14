64ccc86d7b0d844b30f0ca1cc42ede074572a1ad
Chirayu-sopho <chirayuasati19@gmail.com>
2018-02-14 14:43:19 +0530

    Initiated UI and Randomized vehicle path

---
+++ Traffic_Simulation/Assets/ModeOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeOnClick : MonoBehaviour {
+
+    public static bool automatic = true;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changemode()
+    {
+        automatic = !automatic;
+        if(automatic == true)
+        {
+            gameObject.GetComponentInChildren<UnityEngine.UI.Text>().text = "Click for Manual Mode";
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<UnityEngine.UI.Text>().text = "Click for Automatic Mode";
+        }
+    }
+}
--- Traffic_Simulation/Assets/Motion.cs
+++ Traffic_Simulation/Assets/Motion.cs
@@ -4,47 +4,70 @@ using UnityEngine;
 
 public class Motion : MonoBehaviour {
 
-    public GameObject Vehicle;
+    public GameObject[] Vehicles = new GameObject[12];
 
-    public GameObject StartingObject;
-    public GameObject TurningObject;
-    public GameObject TurnedObject;
-    public GameObject StoppingObject;
+    public GameObject[] StartingObjects = new GameObject[8];
+    public GameObject[] TurningObjects = new GameObject[8];
+    public GameObject[] TurnedObjects = new GameObject[8];
+    public GameObject[] StoppingObjects = new GameObject[8];
 
     private int reachturn = 0;
     private int turn = 0;
     private int afterturn = 0;
+    private int vehicleno;
+    private int startingpoint;
+    private int endingpoint;
 	// Use this for initialization
 	void Start () {
-        Vehicle.transform.position = StartingObject.transform.position;
+        if(ModeOnClick.automatic == true)
+        {
+            vehicleno = Random.Range(0, 11);
+            startingpoint = Random.Range(0, 7);
+            endingpoint = Random.Range(0, 7);
+        }
+        Vehicles[vehicleno].transform.position = StartingObjects[startingpoint].transform.position;
+        Vehicles[vehicleno].SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        
         if(reachturn == 0)
         {
-            Vehicle.transform.position = Vehicle.transform.position + (TurningObject.transform.position - StartingObject.transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.position = Vehicles[vehicleno].transform.position + (TurningObjects[startingpoint].transform.position - StartingObjects[startingpoint].transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.LookAt(TurningObjects[startingpoint].transform);
         }
-        if(Mathf.Abs(Vehicle.transform.position.x - TurningObject.transform.position.x) < 0.08)
+        if(distance(Vehicles[vehicleno], TurningObjects[startingpoint]) < 0.08)
         {
             reachturn = 1;
         }
         if (reachturn == 1 && turn == 0)
         {
-            Vehicle.transform.position = Vehicle.transform.position + (TurnedObject.transform.position - TurningObject.transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.position = Vehicles[vehicleno].transform.position + (TurnedObjects[endingpoint].transform.position - TurningObjects[startingpoint].transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.LookAt(TurnedObjects[endingpoint].transform);
         }
-        if (Mathf.Abs(Vehicle.transform.position.z - TurnedObject.transform.position.z) < 0.08)
+        if (distance(Vehicles[vehicleno], TurnedObjects[endingpoint]) < 0.08)
         {
             turn = 1;
         }
         if (reachturn == 1 && turn == 1 && afterturn == 0)
         {
-            Vehicle.transform.position = Vehicle.transform.position + (StoppingObject.transform.position - TurnedObject.transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.position = Vehicles[vehicleno].transform.position + (StoppingObjects[endingpoint].transform.position - TurnedObjects[endingpoint].transform.position) * Time.deltaTime * 0.1f;
+            Vehicles[vehicleno].transform.LookAt(StoppingObjects[endingpoint].transform);
         }
-        if (Mathf.Abs(Vehicle.transform.position.z - StoppingObject.transform.position.z) < 0.08)
+        if (distance(Vehicles[vehicleno], StoppingObjects[endingpoint]) < 0.08)
         {
             afterturn = 1;
         }
+    }
+
+    private float distance(GameObject a, GameObject b)
+    {
+        float x = a.transform.position.x - b.transform.position.x;
+        float y = a.transform.position.y - b.transform.position.y;
+        float z = a.transform.position.z - b.transform.position.z;
+
+        return x * x + y * y + z * z;
     }
 }
